532686ce11b86bda26c11d5e5b800adbe252bbab
FR-Ananas <thcplnd@gmail.com>
2025-04-09 15:28:07 -0400

    Update chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,7 +1,20 @@
 const socket = io();
 const username = localStorage.getItem('username');
 
-socket.emit('login', username);
+if (!username) window.location.href = 'login.html';
+
+socket.emit('login', username, (response) => {
+  if (!response.success) {
+    alert(response.message);
+    window.location.href = 'login.html';
+  }
+});
+
+// Réception initiale des infos
+socket.on('init', ({ users, messages }) => {
+  users.forEach(addUserToList);
+  messages.forEach(m => addMessage(`${m.user}: ${m.text}`));
+});
 
 socket.on('chat-message', ({ user, text }) => {
   addMessage(`${user}: ${text}`);
@@ -9,10 +22,16 @@ socket.on('chat-message', ({ user, text }) => {
 
 socket.on('user-joined', (user) => {
   addMessage(`🟢 ${user} a rejoint le chat`);
+  addUserToList(user);
 });
 
 socket.on('user-left', (user) => {
   addMessage(`🔴 ${user} a quitté le chat`);
+  removeUserFromList(user);
+});
+
+socket.on('update-users', (userList) => {
+  updateUserList(userList);
 });
 
 function sendMessage() {
@@ -30,4 +49,23 @@ function addMessage(msg) {
   div.textContent = msg;
   messages.appendChild(div);
   messages.scrollTop = messages.scrollHeight;
+}
+
+function addUserToList(user) {
+  const userList = document.getElementById('user-list');
+  const li = document.createElement('li');
+  li.id = `user-${user}`;
+  li.textContent = user;
+  userList.appendChild(li);
+}
+
+function removeUserFromList(user) {
+  const el = document.getElementById(`user-${user}`);
+  if (el) el.remove();
+}
+
+function updateUserList(users) {
+  const userList = document.getElementById('user-list');
+  userList.innerHTML = '';
+  users.forEach(addUserToList);
 }
