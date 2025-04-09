29fa0d0bea2f1c6d2b06ebf5e2783e194ba8a7fb
FR-Ananas <thcplnd@gmail.com>
2025-04-09 15:27:41 -0400

    Update socketManager.js

--- server/socketManager.js
+++ server/socketManager.js
@@ -1,23 +1,54 @@
 module.exports = function(io) {
-  const users = {};
+  const users = {};         // socket.id => username
+  const usernames = new Set(); // Pour valider les doublons
+  const messageHistory = []; // Historique des messages
 
   io.on('connection', (socket) => {
-    console.log('🔌 Nouvel utilisateur connecté');
+    console.log('🟢 Nouvelle connexion');
 
-    socket.on('login', (username) => {
+    socket.on('login', (username, callback) => {
+      if (usernames.has(username)) {
+        return callback({ success: false, message: "Pseudo déjà utilisé" });
+      }
+
       users[socket.id] = username;
+      usernames.add(username);
+      console.log(`✅ ${username} connecté`);
+
+      // Envoi de l'historique et de la liste des utilisateurs
+      socket.emit('init', {
+        users: Array.from(usernames),
+        messages: messageHistory
+      });
+
+      // Notifie les autres utilisateurs
       socket.broadcast.emit('user-joined', username);
+      io.emit('update-users', Array.from(usernames));
+
+      callback({ success: true });
     });
 
     socket.on('chat-message', (msg) => {
       const username = users[socket.id];
-      io.emit('chat-message', { user: username, text: msg });
+      if (!username) return;
+
+      const message = { user: username, text: msg };
+      messageHistory.push(message);
+
+      if (messageHistory.length > 100) messageHistory.shift(); // cap historique
+
+      io.emit('chat-message', message);
     });
 
     socket.on('disconnect', () => {
       const username = users[socket.id];
-      socket.broadcast.emit('user-left', username);
-      delete users[socket.id];
+      if (username) {
+        console.log(`🔴 ${username} déconnecté`);
+        usernames.delete(username);
+        delete users[socket.id];
+        socket.broadcast.emit('user-left', username);
+        io.emit('update-users', Array.from(usernames));
+      }
     });
   });
 };
