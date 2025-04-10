d7ae14697498d9538064e2bcb7045fc4f55ef1d6
FR-Ananas <thcplnd@gmail.com>
2025-04-10 15:59:36 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
 const path = require('path');
-const setupSocket = require('./socketManager');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Middlewares
 app.use(express.static(path.join(__dirname, '../public')));
-app.get('/', (req, res) => res.redirect('/login.html'));
 
-setupSocket(io);
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/login.html'));
+});
 
+// Socket manager
+require('./socketManager')(io);
+
+// Launch
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`✅ Serveur lancé sur http://localhost:${PORT}`));
+server.listen(PORT, () => {
+  console.log(`✅ 404Buddy actif sur http://localhost:${PORT}`);
+});
