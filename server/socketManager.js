75d2f3a4a313781921159ff0c1eb75ac1ba88d04
FR-Ananas <thcplnd@gmail.com>
2025-04-11 17:30:32 -0400

    Update socketManager.js

--- server/socketManager.js
+++ server/socketManager.js
@@ -2,24 +2,21 @@ const users = new Map();
 
 module.exports = function (io) {
   io.on("connection", (socket) => {
-    console.log("🟢 Nouveau client connecté :", socket.id);
+    console.log("🟢 Client connecté :", socket.id);
 
-    // Quand un utilisateur rejoint avec ses infos
     socket.on("userJoined", (userData) => {
       users.set(socket.id, userData);
-      console.log("👥 Utilisateur ajouté :", userData);
+      console.log("👥 Connecté :", userData.username);
       io.emit("userList", Array.from(users.values()));
     });
 
-    // Message reçu
     socket.on("message", (data) => {
       console.log("💬 Message de", data.username, ":", data.content);
       io.emit("message", data);
     });
 
-    // Déconnexion
     socket.on("disconnect", () => {
-      console.log("🔴 Client déconnecté :", socket.id);
+      console.log("🔴 Déconnecté :", socket.id);
       users.delete(socket.id);
       io.emit("userList", Array.from(users.values()));
     });
