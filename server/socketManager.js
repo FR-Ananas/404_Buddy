50e4809cae02d05513994cf0bf4c6076e9e385d6
FR-Ananas <thcplnd@gmail.com>
2025-04-11 16:42:10 -0400

    Update socketManager.js

--- server/socketManager.js
+++ server/socketManager.js
@@ -1,1 +1,14 @@
-(socketManager.js avec broadcast des messages)
+module.exports = function (io) {
+  io.on("connection", (socket) => {
+    console.log("🔌 Nouvel utilisateur connecté");
+
+    socket.on("message", (data) => {
+      console.log("📨 Message reçu :", data);
+      io.emit("message", data); // Renvoie à tous les clients
+    });
+
+    socket.on("disconnect", () => {
+      console.log("👋 Utilisateur déconnecté");
+    });
+  });
+};
